7de9774bce733f21707a81f8513c233ba6227028
ChenYue Zhang <CZhang13@slb.com>
2017-10-25 15:50:12 +0800

    remove  prod build source map

--- client-v/build/dev-server.js
+++ client-v/build/dev-server.js
@@ -6,9 +6,19 @@ if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
 
+const mock = process.argv.indexOf('--mock') >= 0;
+const express = require(__dirname + '/../../server/node_modules/express');
+const esession = require(__dirname + '/../../server/node_modules/express-session');
+const logger = require(__dirname + '/../../server/node_modules/morgan');
+const bodyParser = require(__dirname + '/../../server/node_modules/body-parser');
+const passport = require(__dirname + '/../../server/node_modules/passport');
+const ensure = require(__dirname + '/../../server/node_modules/connect-ensure-login');
+const sauthPassport = require(__dirname + '/../../server/auth/sauth.passport');
+const appsettings = require(__dirname + '/../../server/appsettings.json');
+const mockroutes = require(__dirname + '/../mock/routes');
+
 const opn = require('opn')
 const path = require('path')
-const express = require('express')
 const webpack = require('webpack')
 const proxyMiddleware = require('http-proxy-middleware')
 const webpackConfig = (process.env.NODE_ENV === 'testing' || process.env.NODE_ENV === 'production')
@@ -25,41 +35,36 @@ const proxyTable = config.dev.proxyTable
 
 const app = express()
 const compiler = webpack(webpackConfig)
+const devMiddleware = require('webpack-dev-middleware')(compiler, { publicPath: webpackConfig.output.publicPath, quiet: false, stats: { colors: true, modules: false, children: false, chunks: false, chunkModules: false } });
+const hotMiddleware = require('webpack-hot-middleware')(compiler, { log: console.log, heartbeat: 2000 });
+const page = require(__dirname + '/../../server/routes/page');
 
-const devMiddleware = require('webpack-dev-middleware')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  quiet: false
-})
+app.use(logger('dev', { skip: function (req, res) { return res.statusCode < 400; } }));
+app.use(esession({ secret: 'slb dls', resave: false, saveUninitialized: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var configuration = appsettings['Consul'];
+Object.keys(appsettings['Keyvault']).forEach((key) => configuration[key] = appsettings['Keyvault'][key]);
 
-const hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: console.log,
-  heartbeat: 2000
-})
-// force page reload when html-webpack-plugin template changes
-// currently disabled until this is resolved:
-// https://github.com/jantimon/html-webpack-plugin/issues/680
-// compiler.plugin('compilation', function (compilation) {
-//   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-//     hotMiddleware.publish({ action: 'reload' })
-//     cb()
-//   })
-// })
-
+var checkAuth = (req, res, next) => next();
+if (!mock) {
+  sauthPassport(app, passport, configuration);
+  app.use('/api/*', (req, res, next) => { req.appconfig = configuration; next(); });
+  app.use(require(__dirname + '/../../server/routes/api'));
+  checkAuth = ensure.ensureLoggedIn({ redirectTo: '/signon', setReturnTo: true });
+} else {
+  app.use(mockroutes);
+}
 // enable hot-reload and state-preserving
 // compilation error display
 app.use(hotMiddleware)
 
-// proxy api requests
-Object.keys(proxyTable).forEach(function (context) {
-  let options = proxyTable[context]
-  if (typeof options === 'string') {
-    options = { target: options }
-  }
-  app.use(proxyMiddleware(options.filter || context, options))
-})
+// rewrite page route to locate it in memory files
+page.pageRoutes.forEach(function (route) {
+  app.get(route, checkAuth, (req, res, next) => { req.url = '/index.html'; next(); });
+});
 
-// handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')())
+app.get('/credits', checkAuth, (req, res, next) => { req.url = '/credits.html'; next(); });
 
 // serve webpack bundle output
 app.use(devMiddleware)
@@ -87,8 +92,10 @@ devMiddleware.waitUntilValid(() => {
     if (err) {
       _reject(err)
     }
-    process.env.PORT = port
-    var uri = 'http://localhost:' + port
+    process.env.PORT = port;
+    var wellid = process.env.RHAPSODY_DEBUG_TEST_WELLID || '';
+    var qparam = wellid ? '?wellID=' + wellid + '&wellboreID=00000000-0000-0000-0000-000000000000' : '';
+    var uri = 'http://localhost:' + port + qparam;
     console.log('> Listening at ' + uri + '\n')
     // when env is testing, don't need open it
     if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
