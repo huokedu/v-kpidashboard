68b88bd18d9e57eee98280b5e1339a81a8892e0a
ChenYue Zhang <CZhang13@slb.com>
2017-10-26 14:49:13 +0800

    add route scss support

---
+++ client-v/src/views/performance.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="title">{{title}}</div>
+        <router-link to="/qcview">Go to QC</router-link>
+    </div>
+</template>
+<script>
+export default {
+  name: 'performance-view',
+  data () {
+    return {
+      title: 'Performance View'
+    }
+  }
+}
+</script>
+<style lang='scss' scoped>
+    $font-size:40px;
+    .title{
+        font-size:$font-size;
+        font-weight:bold;
+    }
+</style>
